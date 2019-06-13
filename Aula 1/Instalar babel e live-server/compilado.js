2b43702d5b95ceeb996235f41cb30753f36e914f
carinadidomenico <carinadidomenico@gmail.com>
2019-06-13 16:16:37 -0300

    Organizar pastas

--- Aula 1/Instalar babel e live-server/compilado.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-console.log("app funcionando");
-var app = document.getElementById('app');
-var template = React.createElement("h1", null, "AI QUE SONO");
-ReactDOM.render(template, app);
--- Instalar babel e live-server/script.js
+++
@@ -1,7 +0,0 @@
-console.log ("app funcionando")
-
-const app = document.getElementById('app')
-
-const template = <h1>AI QUE SONO</h1>
-
-ReactDOM.render(template, app)
