00684d363da77663e871aaa6474315aea0986762
carinadidomenico <carinadidomenico@gmail.com>
2019-06-14 18:47:21 -0300

    Dados - objetos

---
+++ Aula 4/juntar-compomentes/src/dados/comentarios.js
@@ -0,0 +1,39 @@
+const comentarios = [
+    {
+        nome: "Carina",
+        img: "http://placekitten.com/g/600/600",
+        description: "Gato",
+        subtitulo: "Cats are cute boop boop",
+        comentario: "Ask to go outside and ask to come inside and ask to go outside and ask to come inside run around the house at 4 in the morning trip on catnip give attitude, and chirp at birds."
+    },
+    {
+        nome: "Carina",
+        img: 'http://placekitten.com/g/400/400',
+        descricao: 'Gato',
+        subtitulo: "Cats are cute meow meow",
+        comentario: "I just saw other cats inside the house and nobody ask me before using my litter box love blinks and purr purr purr purr yawn meow to be let out when owners are asleep, cry for no apparent reason"
+    },
+    {
+        nome: "Carina",
+        img: "http://placekitten.com/g/600/600",
+        description: "Gato",
+        subtitulo: "Cats are cute boop boop",
+        comentario: "Ask to go outside and ask to come inside and ask to go outside and ask to come inside run around the house at 4 in the morning trip on catnip give attitude, and chirp at birds."
+     },
+     {
+        nome: "Carina",
+        img: 'http://placekitten.com/g/400/400',
+        descricao: 'Gato',
+        subtitulo: "Cats are cute meow meow",
+        comentario: "I just saw other cats inside the house and nobody ask me before using my litter box love blinks and purr purr purr purr yawn meow to be let out when owners are asleep, cry for no apparent reason"
+      }
+  ]
+
+export default comentarios
+
+
+// img="http://placekitten.com/g/600/600"
+// description="Gato"
+// nome="Carina"
+// subtitulo="Cats are cute boop boop"
+// comentario="Ask to go outside and ask to come inside and ask to go outside and ask to come inside run around the house at 4 in the morning trip on catnip give attitude, and chirp at birds."
---
+++ Aula 4/juntar-compomentes/src/dados/previsoes.js
@@ -0,0 +1,44 @@
+const previsoes = [
+    {
+      data: '31/05/2019',
+      resumo: 'Ensolarado',
+      imagem: 'https://www.onlygfx.com/wp-content/uploads/2018/09/4-clipart-sun-1.png',
+      descricao: "Sol",
+      temperatura: {
+        max: 31,
+        min: 20
+      }
+    },
+    {
+      data: '01/06/2019',
+      resumo: 'Nublado',
+      imagem: 'https://cdn.pixabay.com/photo/2013/04/01/09/22/clouds-98536_960_720.png',
+      descricao: "Nuvens",
+      temperatura: {
+        max: 25,
+        min: 18
+      }
+    },
+    {
+      data: '14/06/2019',
+      resumo: 'Nublado',
+      imagem: 'https://cdn.pixabay.com/photo/2013/04/01/09/22/clouds-98536_960_720.png',
+      descricao: "Nuvens",
+      temperatura: {
+        max: 29,
+        min: 10
+      }
+     },
+     {
+      data: '31/07/2019',
+      resumo: 'Nevando',
+      imagem: 'https://image.flaticon.com/icons/svg/1200/1200430.svg',
+      descricao: "Neve",
+      temperatura: {
+        max: 10,
+        min: 0
+      }
+     }
+  ]
+
+export default previsoes
