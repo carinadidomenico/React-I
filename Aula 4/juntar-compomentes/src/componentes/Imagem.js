1a4317de21c84561a49814151bfcf254b928c7fc
carinadidomenico <carinadidomenico@gmail.com>
2019-06-14 18:48:00 -0300

    Componentes

--- Aula 4/juntar-compomentes/src/componentes/Imagem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Imagem = (props) => {
-    return (
-        <img className="previsao__img" src={props.imagem} alt={props.descricao}/>
-    )
-}
-
-export default Imagem
--- Aula 4/juntar-compomentes/src/index.js
+++ Aula 4/juntar-compomentes/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dados from "./componentes/Dados"
+import Dados from "./componentes/previsão/Dados"
+import TamanhoFonte from "./componentes/fonte/TamanhoFonte"
 import * as serviceWorker from './serviceWorker';
+import Comentarios from './componentes/comentarios/Comentarios'
+import comentarios from "./dados/comentarios";
 
-ReactDOM.render(<Dados/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <Dados/>
+                <TamanhoFonte/>
+                <Comentarios/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+        
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
